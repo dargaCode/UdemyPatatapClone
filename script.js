3b4f14abd61ade43a4540699eac0117ea2861562
dargaCode <darga.code@gmail.com>
2016-04-02 20:07:30 -0700

    Refactor object to contain sub-object for each key
    So that the object can contain a key for color as well

--- script.js
+++ script.js
@@ -2,33 +2,33 @@
 
 var RADIUS = 500;
 
-var SOUNDS = {
-  "q": "sounds/bubbles.mp3",
-  "w": "sounds/clay.mp3",
-  "e": "sounds/confetti.mp3",
-  "r": "sounds/corona.mp3",
-  "t": "sounds/dotted-spiral.mp3",
-  "y": "sounds/flash-1.mp3",
-  "u": "sounds/flash-2.mp3",
-  "i": "sounds/flash-3.mp3",
-  "o": "sounds/glimmer.mp3",
-  "p": "sounds/moon.mp3",
-  "a": "sounds/pinwheel.mp3",
-  "s": "sounds/piston-1.mp3",
-  "d": "sounds/piston-2.mp3",
-  "f": "sounds/piston-3.mp3",
-  "g": "sounds/prism-1.mp3",
-  "h": "sounds/prism-2.mp3",
-  "j": "sounds/prism-3.mp3",
-  "k": "sounds/splits.mp3",
-  "l": "sounds/squiggle.mp3",
-  "z": "sounds/strike.mp3",
-  "x": "sounds/suspension.mp3",
-  "c": "sounds/timer.mp3",
-  "v": "sounds/ufo.mp3",
-  "b": "sounds/veil.mp3",
-  "n": "sounds/wipe.mp3",
-  "m": "sounds/zig-zag.mp3"
+var KEYS = {
+  "q": {sound: "sounds/bubbles.mp3"},
+  "w": {sound: "sounds/clay.mp3"},
+  "e": {sound: "sounds/confetti.mp3"},
+  "r": {sound: "sounds/corona.mp3"},
+  "t": {sound: "sounds/dotted-spiral.mp3"},
+  "y": {sound: "sounds/flash-1.mp3"},
+  "u": {sound: "sounds/flash-2.mp3"},
+  "i": {sound: "sounds/flash-3.mp3"},
+  "o": {sound: "sounds/glimmer.mp3"},
+  "p": {sound: "sounds/moon.mp3"},
+  "a": {sound: "sounds/pinwheel.mp3"},
+  "s": {sound: "sounds/piston-1.mp3"},
+  "d": {sound: "sounds/piston-2.mp3"},
+  "f": {sound: "sounds/piston-3.mp3"},
+  "g": {sound: "sounds/prism-1.mp3"},
+  "h": {sound: "sounds/prism-2.mp3"},
+  "j": {sound: "sounds/prism-3.mp3"},
+  "k": {sound: "sounds/splits.mp3"},
+  "l": {sound: "sounds/squiggle.mp3"},
+  "z": {sound: "sounds/strike.mp3"},
+  "x": {sound: "sounds/suspension.mp3"},
+  "c": {sound: "sounds/timer.mp3"},
+  "v": {sound: "sounds/ufo.mp3"},
+  "b": {sound: "sounds/veil.mp3"},
+  "n": {sound: "sounds/wipe.mp3"},
+  "m": {sound: "sounds/zig-zag.mp3"}
 };
 
 // VARIABLES
@@ -39,8 +39,8 @@ var circles = [];
 
 function onKeyDown(event) {
   var key = event.key;
-  if (SOUNDS[key]) {
-    soundPath = SOUNDS[key];
+  if (KEYS[key]) {
+    soundPath = KEYS[key].sound;
     playSoundFromPath(soundPath);
     addRandomCircle();
   }
